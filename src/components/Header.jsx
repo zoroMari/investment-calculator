44d2b98433b756d9353f791a3645da5227ac4089
Mariia Moroz <mashanya.kov@gmail.com>
2023-10-28 12:54:57 +0200

    first commit with main structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <div id='header'>
+      <img alt='logo' src='investment-calculator-logo.png' />
+      <h1>Investment Calculator</h1>
+    </div>
+  )
+}
